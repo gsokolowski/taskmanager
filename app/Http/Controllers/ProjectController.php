9af2a374de208f94721fe0e47a9971c0c2e94ec5
gsokolowski <gregor.sokolowski@gmail.com>
2024-08-14 14:23:39 +0200

    project with task list, delete project and show

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -4,24 +4,45 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreProjectRequest;
 use App\Http\Requests\UpdateProjectRequest;
+use App\Http\Resources\ProjectCollection;
 use App\Http\Resources\ProjectResource;
 use App\Models\Project;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Spatie\QueryBuilder\QueryBuilder;
 
 class ProjectController extends Controller
 {
-    public function store(StoreProjectRequest $request) {
+    // {{DOMAIN}}/api/projects/1
+    public function show(Project $project) {
+        return (new ProjectResource($project))->load('tasks'); // add tasks to ptoject
+    }
 
+    // {{DOMAIN}}/api/projects?include=tasks
+    public function index(Request $request) {
+
+        $projects = QueryBuilder::for(Project::class)
+            ->allowedIncludes('tasks')
+            ->paginate();
+
+        return new ProjectCollection($projects);
+        //return new ProjectCollection(Auth::user()->projects()->paginate()); // return a collection of projects
+    }
+
+    public function store(StoreProjectRequest $request) {
         $validated = $request->validated();
         $project = Auth::user()->projects()->create($validated);
-        return new ProjectResource( $project );
-   }
+        return new ProjectResource($project);
+    }
 
-   public function update(UpdateProjectRequest $request, Project $project) {
-
+    public function update(UpdateProjectRequest $request, Project $project) {
         $validated = $request->validated();
         $project->update($validated);
-        return new ProjectResource( $project );
-   }
+        return new ProjectResource($project);
+    }
+
+    public function destroy(Project $project) {
+        $project->delete();
+        return response()->noContent();
+    }
 }
